b0d0702d6d928354369f04dce5747a087b23ced7
gboie <igboie@gmail.com>
2022-01-27 18:54:54 -0500

    modified hello

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Argo CD!'))
 app.listen(3000, () => console.log('Server ready'))
