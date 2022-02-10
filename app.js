21c1b86719e4698f768693b514661af2089d6961
Vinod Mudedla <vinod.mudedla@aa.com>
2022-02-10 17:03:45 -0600

    Updated Hello Argo CD

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD!'))
+app.get('/', (req, res) => res.send('Hello Vinod Argo CD!'))
 app.listen(3000, () => console.log('Server ready'))
